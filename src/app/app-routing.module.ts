960881b5a658cd4884dc6173ddfe09b61edd4f4c
Rodrigo Alexandre <rodrigo.anst@gmail.com>
2023-05-15 20:44:30 -0300

    Add rotas, criacao do CRUD

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,26 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CriarPensamentosComponent } from './componentes/pensamentos/criar-pensamentos/criar-pensamentos.component';
+import { ListarPensamentosComponent } from './componentes/pensamentos/listar-pensamentos/listar-pensamentos.component';
+import { ExcluirPensamentoComponent } from './componentes/pensamentos/excluir-pensamento/excluir-pensamento.component';
+import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: 'listarPensamento', pathMatch: 'full' },
+  { path: 'criarPensamento', component: CriarPensamentosComponent },
+  {
+    path: 'listarPensamento',
+    component: ListarPensamentosComponent,
+  },
+  {
+    path: 'pensamentos/excluirPensamento/:id',
+    component: ExcluirPensamentoComponent,
+  },
+  {
+    path: 'pensamentos/editarPensamento/:id',
+    component: EditarPensamentoComponent,
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
